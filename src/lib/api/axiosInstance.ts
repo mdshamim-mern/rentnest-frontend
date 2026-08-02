9e247882fcb49744db74caa656dbe86972c90a57
Md Shamim <mdshamim.mern@gmail.com>
2026-08-02 13:54:53 +0600

    setup axios interceptors for auth token and 401 redirects

--- src/lib/api/axiosInstance.ts
+++ src/lib/api/axiosInstance.ts
@@ -1,1 +1,69 @@
-cc
+import axios from 'axios';
+
+import { useAuthStore } from '../store/authStore';
+
+
+
+export const axiosInstance = axios.create({
+
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+
+  headers: {
+
+    'Content-Type': 'application/json',
+
+  },
+
+});
+
+
+
+axiosInstance.interceptors.request.use(
+
+  (config) => {
+
+    const token = useAuthStore.getState().token;
+
+    if (token) {
+
+      config.headers.Authorization = `Bearer ${token}`;
+
+    }
+
+    return config;
+
+  },
+
+  (error) => {
+
+    return Promise.reject(error);
+
+  }
+
+);
+
+
+
+axiosInstance.interceptors.response.use(
+
+  (response) => response,
+
+  (error) => {
+
+    if (error.response?.status === 401) {
+
+      useAuthStore.getState().logout();
+
+      if (typeof window !== 'undefined') {
+
+        window.location.href = '/login';
+
+      }
+
+    }
+
+    return Promise.reject(error);
+
+  }
+
+);
